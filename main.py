4ac26795733ddbd906acbe9cf176f754768a3a56
Jim Cochrane <salesmanager@shopperscove.com>
2025-05-25 23:54:15 -0400

    Added some more analytics commands to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def main():
-    print("Hello from supermarket!")
+    print("Supermarket Analysis!\n\n")
     sales = pd.read_csv("sales.csv")
 
     print(sales[200:211])
@@ -12,16 +12,21 @@ def main():
     total_gt_100 = sales[sales['Total'] > 100]
     print(total_gt_100.head(10))
 
-    payment_groups = sales.groupby(["Gender", "Payment"])[["Total"]].sum().reset_index()
+# Get the Total by City
+    payment_groups = sales.groupby(["City"])[["Total"]].sum().reset_index()
 
     print(payment_groups)
 
+# Sales > 100 for New York
     new_york_sales_100 = sales[(sales['Total'] > 100) & (sales['City'] == "NewYork")]
     print(new_york_sales_100)
 
+# Alternative (uncomment to see it work)
+    # print(sales.query('Total>100 & City=="NewYork"'))
+
+# .agg() is a great feature of Pandas as it can add it multiple indicates (mean, min) and you can even include your own
     print(sales['Total'].agg(['mean', 'min', 'max', 'argmax']))
-    # print(sales.query('City=="NewYork"'))
-    print(sales.iloc[47])
-    
+
+
 if __name__ == "__main__":
     main()
